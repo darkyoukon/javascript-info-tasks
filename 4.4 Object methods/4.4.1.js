4c587cf8ce0e64b6d397bab75ea4a34100d231db
darkyoukon <d.chukhil@proton.me>
2026-02-28 23:36:06 +0200

    finished 4.3 - 4.4 lessons, completed tasks for the lesson 4.4

---
+++ 4.4 Object methods/4.4.1.js
@@ -0,0 +1,25 @@
+"use strict";
+alert('The first task: Using "this" in object literal');
+// Here the function makeUser returns an object.
+
+// What is the result of accessing its ref? Why?
+
+function makeUser() {
+  return {
+    name: "John",
+    ref: this,
+    proper_ref() {
+      return this;
+    },
+  };
+}
+
+let user = makeUser();
+
+alert(user.proper_ref().name);
+alert(user.proper_ref.name); // outputs just function name
+alert(user.ref.name); // What's the result?
+
+// Actually there won't be any output
+// 'this' keyword could be used in methods, not in object's properties
+// since 'this' context becomes defined at a func call, not at definition stage
---
+++ 4.4 Object methods/4.4.2.js
@@ -0,0 +1,24 @@
+"use strict";
+alert("The second task: Create a calculator");
+// Create an object calculator with three methods:
+
+// read() prompts for two values and saves them as object properties with names a and b respectively.
+// sum() returns the sum of saved values.
+// mul() multiplies saved values and returns the result.
+
+let calculator = {
+  read() {
+    this.a = Number(prompt("Please enter the first Number value: ", 0));
+    this.b = Number(prompt("Please enter the second Number value: ", 0));
+  },
+  sum: function () {
+    return this.a + this.b;
+  },
+  mul() {
+    return this.a * this.b;
+  },
+};
+
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
